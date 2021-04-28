0f82f4723105264734534989a7989e3fd4d30413
amata7 <adrianmata@live.com>
2021-04-27 22:33:19 -0700

    created associations between models

--- models/index.js
+++ models/index.js
@@ -2,4 +2,29 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.belongsToMany(Comment, {
+    through: {
+        model: Post, 
+        unique: true
+    },
+    as: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    through: {
+        model: Post, 
+        unique: true
+    },
+    as: 'comment_id'
+});
+
 module.exports = { User, Post, Comment };
