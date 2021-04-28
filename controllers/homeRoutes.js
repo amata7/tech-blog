9325f2e77bf2eff2aa4e1ef2ac445ebd1442f879
amata7 <adrianmata@live.com>
2021-04-28 00:16:28 -0700

    added handlebars working with 1 user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  try {
+    // Get all users and JOIN with user data
+    const userData = await User.findAll({
+    });
+
+    // Serialize data so the template can read it
+    const users = userData.map((user) => user.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { users });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
